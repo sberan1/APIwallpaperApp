c93454ce0f23e09336e1a712826437dc96156610
stepan <stepan.beran@outlook.com>
2021-01-12 19:30:05 +0100

    :D

---
+++ ApiUkolv2/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ApiUkolv2.Views;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Analytics;
+namespace ApiUkolv2
+{
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ObrazkyPage());
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios=7aaf386b-2d8a-4570-b778-3f3d86e04239;" +
+                     "uwp={Your UWP App secret here};" +
+                     "android={Your Android App secret here}",
+                     typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ApiUkolv2/Views/ObrazkyPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ApiUkolv2.Model;
+
+namespace ApiUkolv2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ObrazkyPage : ContentPage
+    {
+        PictureLoading obrazky = new PictureLoading();
+        public ObrazkyPage()
+        {
+            InitializeComponent();
+            _ = obrazky.GetFromAPI(DisplayAlert("Error", "There seems to be an error, please check your internet connection.", "OK"));
+            BindingContext = obrazky;
+        }
+        void Item_Tapped_Handler(object sender, ItemTappedEventArgs e)
+        {
+            Page p = new ObrazekPage(e.Item as Obrazky);
+            NavigationPage np = new NavigationPage(p);
+            Application.Current.MainPage.Navigation.PushAsync(np);
+           
+        }
+    }
+}
